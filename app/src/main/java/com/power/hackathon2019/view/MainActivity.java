e8af8174b0853cea48656f84857483a1401a206a
Matt <matthewpower1992@gmail.com>
2019-11-02 23:16:08 -0230

    Add map to main activity

--- app/src/main/java/com/power/hackathon2019/view/MainActivity.java
+++ app/src/main/java/com/power/hackathon2019/view/MainActivity.java
@@ -1,43 +1,46 @@
 package com.power.hackathon2019.view;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
-import android.view.View;
-import android.widget.Button;
-import android.app.Activity;
-import com.power.hackathon2019.R;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.power.hackathon2019.R;
+import com.power.hackathon2019.controller.CityBuilder;
+import com.power.hackathon2019.model.City;
+import com.power.hackathon2019.model.Hydrant;
+import com.power.hackathon2019.model.Marker;
+import com.power.hackathon2019.model.MarkerStatus;
 
-public class MainActivity extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
 
-    Button reportButton;
-    TextView hydrantInfo;
-    RadioGroup radioGroup;
-    RadioButton clear;
-    RadioButton needsClearing;
-    boolean cleared;
-    //initialize map
 
-    protected void onCreate(Bundle savedInstanceState) {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback
+{
+    private boolean cleared;
+    private GoogleMap mMap;
+    private CityBuilder cityBuilder;
+
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        reportButton = findViewById(R.id.button);
-        hydrantInfo = findViewById(R.id.hydrantInfo);
-        radioGroup = findViewById(R.id.radioGroup);
-        clear = findViewById(R.id.radioButton2);
-        needsClearing = findViewById(R.id.radioButton);
-        //initialize map
-
-        //~~~~~~UPDATING HYDRANT STATUS TEXT~~~~~~~~~
-        //if (hydrant is selected on map)
-            //hydrantInfo = *hydrant name* + "\n" + "Coordinates: " + LatLng
-            // + "\n" + *hydrant status? i.e. Clear, Needs Clearing*
-
-
+        Button reportButton = findViewById(R.id.button);
+        TextView hydrantInfo = findViewById(R.id.hydrantInfo);
+        RadioGroup radioGroup = findViewById(R.id.radioGroup);
+        RadioButton clear = findViewById(R.id.radioButton2);
+        RadioButton needsClearing = findViewById(R.id.radioButton);
 
         reportButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,9 +52,15 @@ public class MainActivity extends Activity {
             }
         });
 
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        this.cityBuilder = new CityBuilder();
     }
 
-    public void onRadioButtonClicked(View view) {
+    public void onRadioButtonClicked(View view)
+    {
         boolean check = ((RadioButton) view).isChecked();
 
         // Check which radio button was clicked
@@ -65,5 +74,56 @@ public class MainActivity extends Activity {
                     cleared = true;
                     break;
         }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap)
+    {
+        mMap = googleMap;
+
+        City mountPearl = cityBuilder.buildCity(new LatLng(47.519467, -52.800441));
+
+        moveCamera(mountPearl);
+
+        displayMarkers(googleMap, mountPearl);
+    }
+
+    private void moveCamera(City mountPearl)
+    {
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(mountPearl.getLatLng()));
+        mMap.setMinZoomPreference(12.0f);
+        mMap.setMaxZoomPreference(22.0f);
+    }
+
+    private void displayMarkers(GoogleMap googleMap, City mountPearl)
+    {
+        for (Marker marker : mountPearl.getAllMarkers())
+        {
+            googleMap.addMarker(new MarkerOptions().position(marker.getLatLng())
+                    .title(marker.getMarkerName())
+                    .icon(BitmapDescriptorFactory.fromResource(getBitmap(marker))));
+        }
+    }
+
+    private int getBitmap(Marker marker)
+    {
+        MarkerStatus status = marker.getMarkerStatus();
+
+        if (Hydrant.class.equals(marker.getClass()))
+        {
+            if (MarkerStatus.CLEAR.equals(status))
+            {
+                return R.drawable.hydrant25;
+            }
+            else if (MarkerStatus.SNOW_COVERED.equals(status))
+            {
+                return R.drawable.hydrant25snowy;
+            }
+            else if (MarkerStatus.CLEAR_HIGHLIGHTED.equals(status))
+            {
+                return R.drawable.hydrant25highlighted;
+            }
+        }
+        return R.drawable.hydrant25;
     }
 }
